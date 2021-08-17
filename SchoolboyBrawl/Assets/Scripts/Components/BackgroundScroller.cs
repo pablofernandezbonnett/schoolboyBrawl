5bce47ef32db61df5f4c8f2f2d280eee8688ee1d
pablo.mfb <pmiguel.ieee@gmail.com>
2021-08-17 22:32:55 +0100

    changes
    Assets cleanup
    New school inside corridor scene
    New city scene
    nothing else yet

--- SchoolboyBrawl/Assets/Scripts/Components/BackgroundScroller.cs
+++ SchoolboyBrawl/Assets/Scripts/Components/BackgroundScroller.cs
@@ -4,17 +4,17 @@ public class BackgroundScroller : MonoBehaviour
 {
 
     [SerializeField] private GameObject camera;
-    [SerializeField] private GameObject backgroundTemplate;
+    [SerializeField] private GameObject background;
 
     private float _startPosition;
     // con boxcollider 2D
-    private BoxCollider2D _boxCollider2D;
+    private BoxCollider _boxCollider;
     private Camera _camera;
     
     void Start()
     {
         _startPosition = transform.position.x;
-        _boxCollider2D = GetComponent<BoxCollider2D>();
+        _boxCollider = GetComponent<BoxCollider>();
         _camera = camera.GetComponent<Camera>();
     }
 
@@ -26,15 +26,15 @@ public class BackgroundScroller : MonoBehaviour
     private void EndlessBackground()
     {
         float cameraPositionX = camera.transform.position.x;
-        float boxCollider2DLengthX = _boxCollider2D.size.x;
-        float limit = _startPosition + boxCollider2DLengthX;
+        float boxColliderLengthX = _boxCollider.size.x;
+        float limit = _startPosition + boxColliderLengthX;
         float offset = _camera.orthographicSize;
 
         if (cameraPositionX >= limit - offset * 2)
         {
             Debug.Log("cameraPositionX => " + cameraPositionX);
             var position = new Vector3(limit, transform.position.y, transform.position.z);
-            var newInstance = Instantiate(backgroundTemplate, position, transform.rotation);
+            var newInstance = Instantiate(background, position, transform.rotation);
             Debug.Log("newInstance name => " + newInstance.name);
             Debug.Log("this objetc name => " + this.name);
             Destroy(gameObject);
